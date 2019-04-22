14c5a0e79b34004196f67e51c0e43e4ae22badac
Cesar <cnavarro1112@gmail.com>
2019-04-21 22:56:28 -0500

    formato

--- sunat/json.go
+++ sunat/json.go
@@ -7,23 +7,23 @@ import (
 )
 
 type Ruc struct {
-	Ruc                    string        `json:"sunat"`
-	RazonSocial            string        `json:"razon_social"`
-	Ciiu                   string        `json:"ciiu"`
-	FechaActividad         string        `json:"fecha_actividad"`
-	ContribuyenteCondicion string        `json:"contribuyente_condicion"`
-	ContribuyenteTipo      string        `json:"contribuyente_tipo"`
-	ContribuyenteEstado    string        `json:"contribuyente_estado"`
-	NombreComercial        string        `json:"nombre_comercial"`
-	FechaInscripcion       string        `json:"fecha_inscripcion"`
-	DomicilioFiscal        string        `json:"domicilio_fiscal"`
-	SistemaEmision         string        `json:"sistema_emision"`
-	SistemaContabilidad    string        `json:"sistema_contabilidad"`
-	ActividadExterior      string        `json:"actividad_exterior"`
-	EmisionElectronica     string        `json:"emision_electronica"`
-	FechaInscripcionPle    string        `json:"fecha_inscripcion_ple"`
-	Oficio                 string        `json:"Oficio"`
-	FechaBaja              string        `json:"fecha_baja"`
+	Ruc                    string `json:"sunat"`
+	RazonSocial            string `json:"razon_social"`
+	Ciiu                   string `json:"ciiu"`
+	FechaActividad         string `json:"fecha_actividad"`
+	ContribuyenteCondicion string `json:"contribuyente_condicion"`
+	ContribuyenteTipo      string `json:"contribuyente_tipo"`
+	ContribuyenteEstado    string `json:"contribuyente_estado"`
+	NombreComercial        string `json:"nombre_comercial"`
+	FechaInscripcion       string `json:"fecha_inscripcion"`
+	DomicilioFiscal        string `json:"domicilio_fiscal"`
+	SistemaEmision         string `json:"sistema_emision"`
+	SistemaContabilidad    string `json:"sistema_contabilidad"`
+	ActividadExterior      string `json:"actividad_exterior"`
+	EmisionElectronica     string `json:"emision_electronica"`
+	FechaInscripcionPle    string `json:"fecha_inscripcion_ple"`
+	Oficio                 string `json:"Oficio"`
+	FechaBaja              string `json:"fecha_baja"`
 }
 
 func RucJson(nroRuc string) Ruc {
